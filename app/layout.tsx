beb15174f3fdba6b149a8c333402a51bb7319b53
MikelCruz <mikel.cruz@ikasle.aeg.eus>
2024-01-03 17:28:03 +0100

    [UPDATE] Modificado el layout implementando las nuevas navegaciones

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,36 @@
 import { Figtree } from 'next/font/google'
+
 import Sidebar from '@/components/Sidebar'
+import ToasterProvider from '@/providers/ToasterProvider'
+import ModalProvider from '@/providers/ModalProvider'
+import SupabaseProvider from '@/providers/SupabaseProvider'
+
 import './globals.css'
 
 const font = Figtree({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Spotify Clone',
-  description: 'Listen to music!',
+  description: 'Spotify Clone',
 }
+
+export const revalidate = 0;
 
 export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+
   return (
     <html lang="en">
       <body className={font.className}>
-      <Sidebar>
-        {children}
-      </Sidebar>
+        <ToasterProvider />
+        <SupabaseProvider>
+            <Sidebar>
+              {children}
+            </Sidebar>
+        </SupabaseProvider>
       </body>
     </html>
   )
