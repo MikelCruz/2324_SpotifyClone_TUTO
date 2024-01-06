c4d8e6bc1f6dd23f814073bbc235adce94a9ad20
MikelCruz <mikel.cruz@ikasle.aeg.eus>
2024-01-06 12:44:50 +0100

    [FIX] Correccion de rutas de archivo next.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: [
       "mvtzafdnoqacqytqmsje.supabase.co"
     ]
-  }
+  },
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, '.'); // Ajusta la ruta según tu estructura de carpetas.
+    return config;
+  },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
