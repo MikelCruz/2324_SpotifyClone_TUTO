efe93948d72c864df0f890c3e22ecfb5eff17f70
MikelCruz <mikel.cruz@ikasle.aeg.eus>
2024-01-06 12:58:29 +0100

    [TEST] Eliminado el test anterior de nextconfig.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,10 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: [
       "mvtzafdnoqacqytqmsje.supabase.co"
     ]
-  },
-  webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname, '..'); // Ajusta la ruta según tu estructura de carpetas.
-    return config;
-  },
+  }
 }
 
-module.exports = nextConfig;
+module.exports = nextConfig
