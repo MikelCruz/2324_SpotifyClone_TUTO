b87d1fa00224a1cd91570caf055cdfd30a2ab8fd
MikelCruz <mikel.cruz@ikasle.aeg.eus>
2024-01-06 12:46:54 +0100

    [TEST] añadido .

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ const nextConfig = {
     ]
   },
   webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname, '.'); // Ajusta la ruta según tu estructura de carpetas.
+    config.resolve.alias['@'] = path.resolve(__dirname, '..'); // Ajusta la ruta según tu estructura de carpetas.
     return config;
   },
 }
